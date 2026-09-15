114679db5afb6aab0df81472161d4fadd0986ec1
KeySpirit Dev <keyspirit@dev.local>
2026-09-15 02:46:56 +0000

    v1.13.2: v1+v2双签名，修复安装失败问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.keyspirit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 47
-        versionName = "1.13.1"
+        versionCode = 48
+        versionName = "1.13.2"
     }
 
     signingConfigs {
