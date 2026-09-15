468bee67c8c63529c455bd8fadf1245872f67f86
KeySpirit Dev <keyspirit@dev.local>
2026-09-15 02:18:28 +0000

    添加APK签名配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,19 @@ android {
         versionName = "1.13.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release-keystore.jks")
+            storePassword = "keyspirit123"
+            keyAlias = "keyspirit"
+            keyPassword = "keyspirit123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
